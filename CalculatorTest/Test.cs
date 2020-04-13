a826b9635afdef2e4d3c169cf8b307e6cc4a2cc1
Aleksandra <izhukharevich@icloud.com>
2020-04-13 20:47:22 +0300

    add all files

---
+++ CalculatorTest/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculatorTest
+{
+    using Calculator;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestMin()
+        {
+            int min;
+            min = CalculatorClass.Min(3, 4, 5);
+            Assert.AreEqual(2, min);
+        }
+    }
+}
